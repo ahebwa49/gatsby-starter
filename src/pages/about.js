0a25c126e5a249ef5103f56a3747db5034a41b52
Livingstone Asabahebwa <ahebwa49@gmail.com>
2019-01-14 18:28:35 +0300

    add a header instance

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,7 @@ import Header from "../components/header"
 
 export default () => (
   <div style={{ color: "teal" }}>
-    <h1> About gatsby </h1>
+    <Header />
     <p> Such wow. very React</p>
   </div>
 );
