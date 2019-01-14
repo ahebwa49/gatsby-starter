634f6ccf990067e0056531b819b116e620fbbc12
Livingstone Asabahebwa <ahebwa49@gmail.com>
2019-01-14 17:55:24 +0300

    add some content to the div

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,8 @@
-import React from "react"
+import React from "react";
 
 export default () => (
-  <div style = {{ color: 'teal' }}>
-
+  <div style={{ color: "teal" }}>
+    <h1> About gatsby </h1>
+    <p> Such wow. very React</p>
   </div>
 );
