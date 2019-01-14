8f9dddea6f7c414d3f20f8ff133bdd1aa32475f7
Livingstone Asabahebwa <ahebwa49@gmail.com>
2019-01-14 19:23:17 +0300

    add a link component

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Link } from "gatsby"
 
 export default () => (
   <div style={{ color: `purple` }}>
+  <Link to = "/contact/">Contact</Link>
     <Header headerText = "Hello Gatsby!" />
     <p> what a world! </p>
     <img src="https://source.unsplash.com/random/400x200" alt="dummy image" />
