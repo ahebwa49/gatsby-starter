c9fbc6dab37d51137a18df64caab6e2ba2c84d03
Livingstone Asabahebwa <ahebwa49@gmail.com>
2019-01-14 19:38:00 +0300

    add an instance of the header component with some header text

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,6 +5,7 @@ import Header from "../components/header"
 export default () => (
   <div style ={{ color: 'teal' }}>
     <Link to = "/"> Home </Link>
+    <Header headerText = "Contact" />
 
   </div>
 )
